062c7d847f08081e3e1150a8ca2bfb1590504897
David Suilea <david.suilea@gmail.com>
2017-07-13 18:48:01 +0200

    Make it work via network

--- server.js
+++ server.js
@@ -4,31 +4,27 @@ var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
 
 server.listen(process.env.PORT || 8081,function(){
-  console.log('Listening on '+server.address().port);
+  log('Listening on '+server.address().port);
 });
 
 server.lastPlayerID = 0; // Keep track of the last id assigned to a new player
 
 io.on('connection', function (socket) {
-  log('new connection');
-  socket.emit('allExistingPlayers', getAllPlayers());
-
   socket.on('register', function () {
-    log('new registration')
     socket.player = {
       id: server.lastPlayerID++,
       x: randomInt(100, 400),
       y: randomInt(100, 400)
     };
     socket.broadcast.emit('newPlayer', socket.player);
-    socket.emit('registered', socket.player);
+    socket.emit('allPlayers', getAllPlayers());
   });
-
-  socket.on('move', function(newPlayerData){
-    socket.player.x = newPlayerData.x;
-    socket.player.y = newPlayerData.y;
-
-    io.sockets.emit('playerMoved', socket.player);
+  socket.on('move', function(direction){
+    var data = {'direction': direction, 'id': socket.player.id}
+    io.sockets.emit('playerMoved', data);
+  });
+  socket.on('fire', function(){
+    io.sockets.emit('playerShoot', socket.player.id);
   });
 });
 
@@ -44,6 +40,7 @@ function getAllPlayers() {
 function randomInt(low, high) {
   return Math.floor(Math.random() * (high - low) + low);
 }
+
 function log(msg) {
   console.log(msg);
 }
--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import Phaser from 'phaser'
 import io from '../node_modules/socket.io-client';
 
 
-var sprite, weapon, cursors, fireButton, game;
-var players = [];
+var cursors, fireButton, game;
+var gameOn = false;
+var players = {};
 ///////////////////////////the client///////////////////////////////////////////
 var client = {}
 function connect() {
   client.socket = io.connect('http://localhost:8081');
-  client.socket.on('allExistingPlayers', getExistingPlayers);
-  client.socket.on('registered', registerPlayer);
+  client.socket.on('newPlayer', addNewPlayer);
+  client.socket.on('allPlayers', addAllPlayers);
+  client.socket.on('playerMoved', movePlayer);
+  client.socket.on('playerShoot', shootPlayer);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -19,6 +22,7 @@ function connect() {
 ///////////////////////////////////////////////////////////////////////////////
 game = new Phaser.Game(800, 600, Phaser.AUTO, '',
   {
+    init: init,
     preload: preload,
     create: create,
     update: update,
@@ -26,69 +30,114 @@ game = new Phaser.Game(800, 600, Phaser.AUTO, '',
   }
 );
 ///////////////////////////////////////////////////////////////////////////////
+function init() {
+    game.stage.disableVisibilityChange = true;
+}
+///////////////////////////////////////////////////////////////////////////////
 function preload() {
   game.load.image('bullet', 'assets/shmup-bullet.png');
   game.load.image('ship', 'assets/thrust_ship.png');
 }
 ///////////////////////////////////////////////////////////////////////////////
 function create() {
+  cursors = this.input.keyboard.createCursorKeys();
+  fireButton = this.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
+
+  connect();
+  client.socket.emit('register');
+}
+///////////////////////////////////////////////////////////////////////////////
+function update() {
+  if(gameOn){
+    if (cursors.up.isDown) {
+      client.socket.emit('move', 'forward');
+    }
+    else {
+      client.socket.emit('move', 'stop');
+    }
+
+    if (cursors.left.isDown) {
+      client.socket.emit('move', 'left');
+    }
+    else if (cursors.right.isDown) {
+      client.socket.emit('move', 'right');
+    }
+    else {
+      client.socket.emit('move', 'none');
+    }
+
+    if (fireButton.isDown) {
+      client.socket.emit('fire');
+    }
+  }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+function render() {
+  //weapon.debug();
+}
+///////////////////////////////////////////////////////////////////////////////
+function addNewPlayer(data) {
+  var sprite = game.add.sprite(data.x, data.y, 'ship');
   //  Creates 30 bullets, using the 'bullet' graphic
-  weapon = game.add.weapon(30, 'bullet');
+  var weapon = game.add.weapon(30, 'bullet');
+
   //  The bullet will be automatically killed when it leaves the world bounds
   weapon.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
   //  The speed at which the bullet is fired
   weapon.bulletSpeed = 600;
   //  Speed-up the rate of fire, allowing them to shoot 1 bullet every 60ms
   weapon.fireRate = 100;
-  
-  //  Tell the Weapon to track the 'player' Sprite
-  //  With no offsets from the position
-  //  But the 'true' argument tells the weapon to track sprite rotation
-  cursors = this.input.keyboard.createCursorKeys();
-  fireButton = this.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
-  connect();
-  client.socket.emit('register');
-}
-///////////////////////////////////////////////////////////////////////////////
-function update() {
-  if (sprite) {
-    if (cursors.up.isDown) {
-      game.physics.arcade.accelerationFromRotation(sprite.rotation, 300, sprite.body.acceleration);
-    }
-    else {
-      sprite.body.acceleration.set(0);
-    }
-    if (cursors.left.isDown) {
-      sprite.body.angularVelocity = -300;
-    }
-    else if (cursors.right.isDown) {
-      sprite.body.angularVelocity = 300;
-    }
-    else {
-      sprite.body.angularVelocity = 0;
-    }
-    if (fireButton.isDown) {
-      weapon.fire();
-    }
-    game.world.wrap(sprite, 16);
-  }
-}
-///////////////////////////////////////////////////////////////////////////////
-function render() {
-  //weapon.debug();
-}
-///////////////////////////////////////////////////////////////////////////////
-function registerPlayer(data) {
-  sprite = game.add.sprite(data.x, data.y, 'ship');
+
   sprite.anchor.set(0.5);
   game.physics.arcade.enable(sprite);
   sprite.body.drag.set(70);
   sprite.body.maxVelocity.set(200);
+  //  Tell the Weapon to track the 'sprite'
+  //  With no offsets from the position
+  //  But the 'true' argument tells the weapon to track sprite rotation
   weapon.trackSprite(sprite, 0, 0, true);
+
+  players[data.id] = {'sprite': sprite, 'weapon': weapon};
 }
 ///////////////////////////////////////////////////////////////////////////////
-function getExistingPlayers(data) {
-  data.forEach(function (player) {
-    game.add.sprite(player.x, player.y, 'ship');
-  })
+function addAllPlayers(allPlayers) {
+  allPlayers.forEach(function(data) {
+    addNewPlayer(data);
+  });
+  gameOn = true;
+}
+///////////////////////////////////////////////////////////////////////////////
+function movePlayer(data) {
+  var direction = data['direction'];
+  var sprite = players[data.id].sprite;
+
+  switch(direction){
+    case "forward":
+      game.physics.arcade.accelerationFromRotation(sprite.rotation, 300, sprite.body.acceleration);
+      break;
+    case 'stop':
+      sprite.body.acceleration.set(0);
+      break;
+    case 'left':
+      sprite.body.angularVelocity = -300;
+      break;
+    case 'right':
+      sprite.body.angularVelocity = 300;
+      break;
+    case 'none':
+      sprite.body.angularVelocity = 0;
+      break;
+  }
+
+  game.world.wrap(sprite, 16);
+}
+///////////////////////////////////////////////////////////////////////////////
+function shootPlayer(id) {
+  var weapon = players[id].weapon
+  var sprite = players[id].sprite
+
+  weapon.fire();
+
+  game.world.wrap(sprite, 16);
 }
